be61f07961b66818dc6799c9c41f80266b368467
ketler888-cmyk <maliivan1989@gmail.com>
2026-02-28 14:03:20 +0200

    chore: code-only mirror (exclude data/venv/logs/results)

--- _sources/GeneTrader/utils/file_operations.py
+++
@@ -1,8 +0,0 @@
-import os
-from utils.logging_config import logger
-
-def create_directories(dirs):
-    for dir_path in dirs:
-        if not os.path.exists(dir_path):
-            os.makedirs(dir_path)
-            logger.info(f"Created directory: {dir_path}")
